4c9c5acd6e744e94d9cdc545937f87013e82d573
Fraterne Bahirwe <fraterne2023@gmail.com>
2026-08-03 13:32:39 +0200

    updating my app.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
 
 // Add react-router-dom imports
-import { Router, RouterProvider } from 'react-router-dom';  
+import { RouterProvider, createBrowserRouter, createRoutesFromElements,Route } from 'react-router-dom';  
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter (createRoutesFromElements (<Route path="/" element={ <Root/>}></Route>))
 
 function App() {
   return (
