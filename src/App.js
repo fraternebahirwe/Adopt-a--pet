d401eebe424115eb49c0924f7889600373fb11d9
Fraterne Bahirwe <fraterne2023@gmail.com>
2026-08-03 13:18:28 +0200

    updating my app.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
 
 // Add react-router-dom imports
-
+import { Router, RouterProvider } from 'react-router-dom';  
 // create router with JSX Route elements
 const appRouter = "REPLACE ME";
 
 function App() {
   return (
     // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <RouterProvider router= {appRouter} />
   );
 }
 
